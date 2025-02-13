2b8d4671265b05abc60cda64d0b369a6e782cc67
Sergio Rebollo Ortega <srebollo2tic@gmail.com>
2025-02-13 09:02:22 +0000

    Config y despliegue add

--- app/btp_proyecto_fi.solicitudadmin/dist/Component-dbg.js
+++ app/btp_proyecto_fi.solicitudadmin/dist/Component-dbg.js
@@ -3,7 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
-        return Component.extend(".tiposolicitudes.Component", {
+        return Component.extend("btpproyectofi.solicitudadmin.Component", {
             metadata: {
                 manifest: "json"
             }
--- app/btp_proyecto_fi.solicituduser/dist/Component-dbg.js
+++ app/btp_proyecto_fi.solicituduser/dist/Component-dbg.js
@@ -3,7 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
-        return Component.extend(".tiposolicitudes.Component", {
+        return Component.extend("btpproyectofi.solicituduser.Component", {
             metadata: {
                 manifest: "json"
             }
